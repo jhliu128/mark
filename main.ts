c6ca259f87c78bfeddde8d36a8053d1da0b08e24
jhliu128 <56717051+jhliu128@users.noreply.github.com>
2019-10-21 15:01:17 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -2,76 +2,6 @@
 
 namespace Music {
     /**
-    * 演奏 魯冰花
-    */
-    //% blockId="Lupinus" block="play Lupinus"
-    //% blockGap=2 weight=1
-    export function moon(): void {
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Half))
-    music.playTone(523, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(247, music.beat(BeatFraction.Half))
-    music.playTone(220, music.beat(BeatFraction.Half))
-    music.playTone(196, music.beat(BeatFraction.Half))
-    music.playTone(247, music.beat(BeatFraction.Half))
-    music.playTone(220, music.beat(BeatFraction.Double))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(370, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Half))
-    music.playTone(370, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(262, music.beat(BeatFraction.Half))
-    music.playTone(262, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(370, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Double))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(440, music.beat(BeatFraction.Half))
-    music.playTone(523, music.beat(BeatFraction.Whole))
-    music.playTone(440, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(392, music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.rest(music.beat(BeatFraction.Half))
-    music.playTone(330, music.beat(BeatFraction.Half))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(294, music.beat(BeatFraction.Whole))
-    music.playTone(247, music.beat(BeatFraction.Half))
-    music.playTone(220, music.beat(BeatFraction.Half))
-    music.playTone(196, music.beat(BeatFraction.Half))
-    music.playTone(247, music.beat(BeatFraction.Half))
-    music.playTone(220, music.beat(BeatFraction.Double))
-    }
-
-
-    /**
     * 演奏 母親像月亮
     */
     //% blockId="moon" block="play moon"
@@ -108,6 +38,7 @@ namespace Music {
     music.playTone(247, music.beat(BeatFraction.Whole))
     music.playTone(196, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Double))
+ 
     }
 
 
@@ -154,5 +85,75 @@ namespace Music {
     music.playTone(294, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Double))
     music.rest(music.beat(BeatFraction.Whole))
+    }
+
+
+    /**
+    * 演奏 魯冰花
+    */
+    //% blockId="lupinus" block="play lupinus"
+    //% blockGap=2 weight=1
+    export function lupinus(): void {
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Double))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(370, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Half))
+    music.playTone(220, music.beat(BeatFraction.Double))
     }
 }

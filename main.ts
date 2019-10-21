82ef7e64c44a85addc7b467f913d40d25b0a94ae
jhliu128 <56717051+jhliu128@users.noreply.github.com>
2019-10-21 08:15:05 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 //% weight=0 color=#3CB371 icon="\uf8ae" block="Music"
+
 namespace Music {
     /**
     * 計算長方形面積，並回傳
@@ -8,12 +9,13 @@ namespace Music {
     export function areaOfRectangle(length: number, width:number): number {
         return length*width
     }
+
     /**
     * 計算長方形面積，不回傳，只顯示在LED
     */
-    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockId="sweat_family" block="play sweat_family"
     //% blockGap=2 weight=1
-    export function ledOfRectangle(length: number, width:number): void {
+    export function ledOfRectangle(): void {
     music.playTone(262, music.beat(BeatFraction.Half))
     music.playTone(262, music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Double))

1f8fe1394e97c901857f20d445af663985ab8b7a
jhliu128 <56717051+jhliu128@users.noreply.github.com>
2019-10-21 10:23:33 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -2,13 +2,45 @@
 
 namespace Music {
     /**
-    * 計算長方形面積，並回傳
+    * 演奏 母親像月亮
     */
-    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
+    //% blockId="moon" block="play moon"
+    //% blockGap=2 weight=1
+    export function moon(): void {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(220, music.beat(BeatFraction.Double))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Double))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(196, music.beat(BeatFraction.Half))
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(196, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Double))
+ 
     }
+
 
     /**
     * 演奏 甜蜜的家庭
@@ -16,7 +48,7 @@ namespace Music {
     //% blockId="sweat_family" block="play sweat_family"
     //% blockGap=2 weight=1
     export function sweat_family(): void {
-music.rest(music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Whole))
     music.playTone(262, music.beat(BeatFraction.Half))
     music.playTone(294, music.beat(BeatFraction.Half))
     music.playTone(330, music.beat(BeatFraction.Whole))
